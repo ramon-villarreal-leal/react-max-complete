5fec74eff4bb747e8a67367f026648f58c4a12f0
Ramon Villarreal Leal <ramon.villarreal.leal@gmail.com>
2020-11-18 23:12:23 -0600

    updating Person.js

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -6,6 +6,7 @@ const person = ( props ) => {
         <div>
             <p>My name is {props.name} and I am {props.age}  years old</p>
             <p>{props.children}</p>
+            <p onClick={props.click}></p>
         </div>
     )
 };
